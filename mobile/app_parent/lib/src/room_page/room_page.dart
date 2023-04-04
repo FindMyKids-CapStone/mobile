617bd3057e6d61c1ffd418a0b514ba349e5f7bea
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2023-04-04 14:17:28 +0700

    update group page

--- mobile/app_parent/lib/src/room_page/room_page.dart
+++ mobile/app_parent/lib/src/room_page/room_page.dart
@@ -1,5 +1,8 @@
+import 'package:app_parent/models/user_model.dart';
 import 'package:app_parent/src/room_page/widget/create_join_room.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import '../../models/group.dart';
 
@@ -11,14 +14,53 @@ class RoomPage extends StatefulWidget {
 }
 
 class _RoomPageState extends State<RoomPage> {
-  List<Group> rooms = [];
+  List<Group> rooms = [
+    Group(
+      id: '001',
+      name: 'Tôi và 6 thằng ngu',
+      createdAt: '02 - 03 - 12',
+      createdBy: 'aa',
+      code: '123',
+      users: <UserModel>[
+        UserModel(displayName: "Khe"),
+        UserModel(displayName: "Híu")
+      ],
+    ),
+    Group(
+      id: '001',
+      name: 'Tôi và 6 thằng ngu',
+      createdAt: '02 - 03 - 12',
+      createdBy: 'aa',
+      code: '123',
+      users: <UserModel>[
+        UserModel(displayName: "Khe"),
+        UserModel(displayName: "Híu")
+      ],
+    ),
+    Group(
+      id: '001',
+      name: 'Tôi và 6 thằng ngu',
+      createdAt: '02 - 03 - 12',
+      createdBy: 'aa',
+      code: '123',
+      users: <UserModel>[
+        UserModel(displayName: "Khe"),
+        UserModel(displayName: "Híu")
+      ],
+    ),
+  ];
   TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Danh sách nhóm"),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true ,
+          title:  Text("Conversations", style: TextStyle(color: Get.isDarkMode ? Colors.white: Colors.black),),
+          leading: IconButton(
+              icon: Icon(Icons.menu, color: Colors.blue,), onPressed: (){}),
           actions: [
             IconButton(
                 onPressed: () {
@@ -26,52 +68,54 @@ class _RoomPageState extends State<RoomPage> {
                       context: context,
                       builder: (context) => DialogCreateJoin(type: "join"));
                 },
-                icon: const Icon(Icons.meeting_room_outlined)),
+                icon: Icon(Icons.meeting_room_outlined, color: Colors.blue,)),
             IconButton(
                 onPressed: () {
                   showDialog(
                       context: context,
                       builder: (context) => DialogCreateJoin(type: "create"));
                 },
-                icon: const Icon(Icons.create))
+                icon: Icon(Icons.create_outlined, color: Colors.blue,)),
+
           ],
         ),
         body: rooms.isEmpty
             ? Container(
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Image(
-                          image: AssetImage("assets/img/rooms-not-found.jpg"),
-                          height: 100),
-                      Text(
-                        "Bạn chưa tham gia nhóm nào",
-                        style: TextStyle(fontSize: 20),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Vui lòng tạo hoặc tham gia nhóm",
-                        textAlign: TextAlign.center,
-                      )
-                    ]),
-              )
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Image(
+                    image: AssetImage("assets/img/rooms-not-found.jpg"),
+                    height: 100),
+                Text(
+                  "Bạn chưa tham gia nhóm nào",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Vui lòng tạo hoặc tham gia nhóm",
+                  textAlign: TextAlign.center,
+                )
+              ]),
+        )
             : ListView.builder(
-                itemCount: rooms.length,
-                itemBuilder: (context, index) => Card(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          ListTile(
-                            leading: Icon(Icons.album),
-                            title: Text('The Enchanted Nightingale'),
-                            subtitle: Text(
-                                'Music by Julie Gable. Lyrics by Sidney Stein.'),
-                          ),
-                        ],
+            itemCount: rooms.length,
+            itemBuilder: (context, index) =>
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      ListTile(
+                        leading: Icon(Icons.album),
+                        title: Text('The Enchanted Nightingale'),
+                        subtitle: Text(
+                            'Music by Julie Gable. Lyrics by Sidney Stein.'),
                       ),
-                    )));
+                    ],
+                  ),
+                )));
   }
 }
