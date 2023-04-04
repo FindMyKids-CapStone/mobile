36cae7b1eacf8e1b8c18260a177591c21bb6dfd3
nmhieu0504 <84891428+nmhieu0504@users.noreply.github.com>
2023-04-04 15:43:52 +0700

    update

--- mobile/app_parent/lib/main.dart
+++ mobile/app_parent/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:app_parent/service/graphql.dart';
 import 'package:app_parent/src/auth_page/auth_page.dart';
-import 'package:app_parent/src/chat_page/chat_page.dart';
 import 'package:app_parent/src/map_page/map_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -23,22 +22,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return GraphQLProvider(
-    //   client: GraphQLHelper.initializeClient(),
-    //   child: StreamBuilder<User?>(
-    //       stream: FirebaseAuth.instance.authStateChanges(),
-    //       builder: (context, snapshot) {
-    //         if (snapshot.connectionState == ConnectionState.waiting) {
-    //           return const Center(child: CircularProgressIndicator());
-    //         } else if (snapshot.hasData) {
-    //           return const GetMaterialApp(
-    //               debugShowCheckedModeBanner: false, home: MapPage());
-    //         } else {
-    //           return const GetMaterialApp(
-    //               debugShowCheckedModeBanner: false, home: AuthPage());
-    //         }
-    //       }),
-    // );
-    return const ChatPage();
+    return GraphQLProvider(
+      client: GraphQLHelper.initializeClient(),
+      child: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasData) {
+              return const GetMaterialApp(
+                  debugShowCheckedModeBanner: false, home: MapPage());
+            } else {
+              return const GetMaterialApp(
+                  debugShowCheckedModeBanner: false, home: AuthPage());
+            }
+          }),
+    );
   }
 }
