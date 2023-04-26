35e00719a7b2c523e8708e8a247ca3876a2a5bcb
Do Tien Trung <dotientrung201030@gmail.com>
2023-04-26 10:34:44 +0700

    update

--- mobile/app_parent/lib/config/backend.dart
+++ mobile/app_parent/lib/config/backend.dart
@@ -1,3 +1,3 @@
 String BACKEND_AUTH_HEADER = "Authorization";
-String BACKEND_HTTP = "https://findmykid.onrender.com";
+String BACKEND_HTTP = "http://34.28.37.205:8080";
 String BACK_END_SOCKET = "http://35.202.63.4:8080";
